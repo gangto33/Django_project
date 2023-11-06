18505a17e343830d7fb89ed56a868d91228eff00
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-11-06 19:46:57 +0900

    Add files via upload

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,25 +1,85 @@
-from django.conf import settings
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import LoginView, LogoutView
-from django.shortcuts import render
-from django.views.generic import CreateView
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from django.contrib.auth.forms import PasswordChangeForm
+from django.contrib.auth import update_session_auth_hash
+from django.contrib import messages
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('/blog/')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('/blog/')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
 
 
-signup = CreateView.as_view(
-    form_class = UserCreationForm,
-    template_name = 'accounts/form.html',
-    success_url = '/accounts/login/'
-)
-
-login = LoginView.as_view(
-    template_name = 'accounts/form.html',
-)
-
-logout = LogoutView.as_view(
-    next_page = '/accounts/login/'
-)
+def logout(request):
+    auth_logout(request)
+    return redirect('/blog/')
 
 @login_required
 def profile(request):
     return render(request, 'accounts/profile.html')
+
+def signup(request):
+    if request.method == 'POST':
+        messages.info(request, "회원가입이 완료되었습니다!")
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/login')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
+
+
+def delete(request):
+    request.user.delete()
+    return redirect('/blog/')
+
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            return redirect('/accounts/profile')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/update.html', context)
+
+
+@login_required
+def change_password(request, user_pk):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            return redirect('/blog/')
+    else:
+        form = PasswordChangeForm(request.user)
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/change_password.html', context)
--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Post(models.Model):
@@ -12,7 +12,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
@@ -67,7 +67,7 @@ class Tag(models.Model):
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
