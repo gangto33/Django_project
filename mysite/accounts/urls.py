f2f44af929efaf8d9a3b94e4e203d9da7ab54be3
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-11-07 15:15:41 +0900

    Update urls.py
    비밀번호 변경 url

--- mysite/accounts/urls.py
+++ mysite/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('delete/', views.delete, name='delete'),
     path('update/', views.update, name='update'),
-    path('profile/', views.profile, name='profile')
+    path('profile/', views.profile, name='profile'),
+    path('change_password/', views.change_password, name="re_password"),
 ]
