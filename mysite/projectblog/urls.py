176080d7853e40304b12ad5b12c876069b4e4e2b
gangto33 <142385671+gangto33@users.noreply.github.com>
2023-11-07 15:20:09 +0900

    Update urls.py
    500 오류 생성

--- mysite/projectblog/urls.py
+++ mysite/projectblog/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+handler500 = 'django.views.defaults.server_error'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
